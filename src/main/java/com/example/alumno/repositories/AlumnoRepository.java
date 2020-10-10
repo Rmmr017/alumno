743a3b933a18b5a12a440cf22d19fb99a2b7cddf
Maximiliano Ruel <rmmr017@gmail.com>
2020-10-10 14:45:43 -0300

    Alumno Repository

---
+++ src/main/java/com/example/alumno/repositories/AlumnoRepository.java
@@ -0,0 +1,9 @@
+package com.example.alumno.repositories;
+
+
+import com.example.alumno.entities.Alumno;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlumnoRepository extends BaseRepository<Alumno, Long> {
+}
