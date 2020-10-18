e505d22a6aabd4d36c984ba6156dd01303b8f1c2
Maximiliano Ruel <rmmr017@gmail.com>
2020-10-18 20:33:01 -0300

    merge de develop a master, cambios realizados, creacion de reopsitorios, clase base y clase Alumno

--- src/main/java/com/example/alumno/entities/Alumno.java
+++ src/main/java/com/example/alumno/entities/Alumno.java
@@ -1,33 +1,14 @@
 package com.example.alumno.entities;
 
-<<<<<<< HEAD
 import javax.persistence.Column;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import java.util.Date;
-
-public class Alumno {
-    @Column(name = "nombre")
-    private String nombre;
-    @Column (name = "apellido")
-    private String apellido;
-    @Column (name = "email")
-    private String email;
-    @Column (name= "create_at")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createAt;
-}
-
-=======
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.envers.Audited;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Column;
-import java.io.Serializable;
-import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -38,15 +19,14 @@ import javax.persistence.Table;
 @Getter
 @Setter
 @Audited
-public class Alumno extends Base implements Serializable {
-
+public class Alumno extends Base {
     @Column(name = "nombre")
-    String nombre;
-    @Column(name = "apellido")
-    String apellido;
-    @Column(name = "email")
-    String email;
-    @Column(name = "createAt")
-    Date createAt;
+    private String nombre;
+    @Column (name = "apellido")
+    private String apellido;
+    @Column (name = "email")
+    private String email;
+    @Column (name= "create_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createAt;
 }
->>>>>>> develop
